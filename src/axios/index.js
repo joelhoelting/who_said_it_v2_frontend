a0e99f433f8f39b2eff52cddfa58f2dee6660f90
Joel Hoelting <joelhoelting@protonmail.com>
2020-10-27 22:19:56 +0100

    Updates deps and adds a mobile menu

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const LOCAL_API_URL = 'http://192.168.1.3:3000/api/v1/';
+const LOCAL_API_URL = 'http://localhost:3000/api/v1/';
 let API_URL = process.env.VUE_APP_API_URL || LOCAL_API_URL;
 
 const plainAxiosInstance = axios.create({
--- src/components/includes/Header/index.vue
+++ src/components/includes/Header/index.vue
@@ -1,6 +1,39 @@
 <template>
   <header>
-    <nav>
+    <div class="inner-mobile-header mobile">
+      <transition name="fade">
+        <page-title v-if="currentRouteName">{{ currentRouteName }}</page-title>
+      </transition>
+      <div class="mobile hamburger-container" @click="mobileMenuActive = !mobileMenuActive">
+        <div class="hamburger-container__hamburger" :class="{ active: mobileMenuActive }">
+          <span class=""></span>
+          <span class=""></span>
+          <span class=""></span>
+          <span class=""></span>
+          <span class=""></span>
+          <span class=""></span>
+        </div>
+      </div>
+    </div>
+    <nav class="mobile mobile-nav" :class="{ active: mobileMenuActive }">
+      <router-link @click.native="closeMobileMenu" class="mobile-nav__home-logo" to="/">
+        <img class="mobile-header-logo" :src="require('@/assets/images/logos/who_said_it_logo.svg')" />
+      </router-link>
+      <div class="mobile-nav__mobile-section">
+        <router-link @click.native="closeMobileMenu" to="/">Home</router-link>
+        <router-link @click.native="closeMobileMenu" to="/play">Play Game</router-link>
+      </div>
+      <div class="mobile-nav__mobile-section" v-if="!isLoggedIn">
+        <router-link @click.native="closeMobileMenu" to="/signin">Sign In</router-link>
+        <router-link @click.native="closeMobileMenu" to="/signup">Sign Up</router-link>
+      </div>
+      <div class="mobile-nav__mobile-section" v-if="isLoggedIn">
+        <router-link @click.native="closeMobileMenu" to="/settings">Settings</router-link>
+        <router-link @click.native="closeMobileMenu" to="/games">Statistics</router-link>
+        <a href="/signout" @click.prevent="signOut">Sign Out</a>
+      </div>
+    </nav>
+    <nav class="desktop desktop-nav">
       <div>
         <router-link to="/">Home</router-link>
         <router-link to="/play">Play Game</router-link>
@@ -31,6 +64,11 @@ export default {
   components: {
     PageTitle
   },
+  data() {
+    return {
+      mobileMenuActive: false
+    };
+  },
   computed: {
     ...mapGetters('authorization', ['isLoggedIn']),
     currentRouteName() {
@@ -38,10 +76,28 @@ export default {
     }
   },
   methods: {
+    closeMobileMenu() {
+      this.mobileMenuActive = false;
+    },
     signOut() {
       this.$store.dispatch('authorization/signOut').then(() => {
         if (this.$route.path !== '/') this.$router.push('/');
       });
+    }
+  },
+  watch: {
+    mobileMenuActive: {
+      handler: function() {
+        console.log('hello');
+        if (this.mobileMenuActive) {
+          document.body.style.overflow = 'hidden';
+        } else {
+          setTimeout(() => {
+            document.body.style.overflow = 'visible';
+          }, 300);
+        }
+      },
+      deep: true
     }
   }
 };
@@ -55,10 +111,140 @@ header {
   top: 0;
   left: 0;
   z-index: 1;
+  transition: height 400ms ease;
+
   @include media-query('tabletLandscape', 'min') {
     height: 100px;
   }
-  nav {
+
+  .inner-mobile-header {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    padding-left: 1em;
+
+    .hamburger-container {
+      height: 60px;
+      width: 60px;
+      position: absolute;
+      right: 0;
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      .hamburger-container__hamburger {
+        z-index: 2;
+        width: 40px;
+        height: 40px;
+        overflow: initial;
+        position: relative;
+        span {
+          display: block;
+          position: absolute;
+          height: 6px;
+          width: 50%;
+          background: #ffffff;
+          opacity: 1;
+          box-shadow: 1px 2px rgba(0, 0, 0, 0.5);
+          transform: rotate(0deg);
+          transition: all 250ms ease-in-out;
+          &:nth-child(2n + 1) {
+            border-radius: 9px 0 0 9px;
+            left: 0px;
+          }
+          &:nth-child(2n) {
+            border-radius: 0 9px 9px 0;
+            left: 50%;
+          }
+          &:nth-child(1),
+          &:nth-child(2) {
+            top: 6px;
+          }
+          &:nth-child(3),
+          &:nth-child(4) {
+            top: 16px;
+          }
+          &:nth-child(5),
+          &:nth-child(6) {
+            top: 26px;
+          }
+        }
+        &.active {
+          span {
+            box-shadow: none;
+            &:nth-child(1) {
+              left: 4px;
+              top: 12px;
+              transform: rotate(45deg);
+            }
+            &:nth-child(2) {
+              left: 16px;
+              top: 12px;
+              transform: rotate(-45deg);
+            }
+            &:nth-child(3) {
+              left: -50%;
+              opacity: 0;
+            }
+            &:nth-child(4) {
+              left: 100%;
+              opacity: 0;
+            }
+            &:nth-child(5) {
+              left: 4px;
+              top: 24px;
+              transform: rotate(-45deg);
+            }
+            &:nth-child(6) {
+              left: 16px;
+              top: 24px;
+              transform: rotate(45deg);
+            }
+          }
+        }
+      }
+    }
+  }
+  .mobile-nav {
+    height: 100%;
+    width: 100%;
+    background: linear-gradient(45deg, #5d37e3 0%, #845df0 100%);
+    transform: translateX(100%);
+    transition: transform 300ms ease;
+    position: fixed;
+    top: 0;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    &.active {
+      transform: translateX(0);
+      .mobile-nav__home-logo {
+        opacity: 1;
+      }
+    }
+    .mobile-nav__home-logo {
+      display: flex;
+      align-items: center;
+      position: absolute;
+      top: 18px;
+      left: 20px;
+      opacity: 0;
+      transition: opacity 300ms ease 150ms;
+      .mobile-header-logo {
+        width: 200px;
+        z-index: 10;
+      }
+    }
+    .mobile-nav__mobile-section {
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+      font-size: 1.4em;
+    }
+  }
+
+  nav.desktop-nav {
     height: 100%;
     display: flex;
     align-items: center;
